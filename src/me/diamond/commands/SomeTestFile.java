70e3d8b694127881d534f5ec3d0a2686bbb4da7e
lukas.vdgaag <lukas.vdgaag@outlook.com>
2020-04-19 15:29:09 +0200

    test file

---
+++ src/me/diamond/commands/SomeTestFile.java
@@ -0,0 +1,9 @@
+package me.diamond.commands;
+
+public class SomeTestFile {
+
+    public void HiThere() {
+       String a = " so i am just typing shit here and you should be able to see this :";
+    }
+
+}
